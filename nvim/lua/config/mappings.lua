2ec1900d123e4ae0b823715bca929ecfde798227
Christian Belloni <christianbelloni@Christians-Laptop.local>
2024-08-19 11:17:40 +0200

    Updated neogit mappings

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -1,1 +1,2 @@
-vim.keymap.set('n', '<Leader>r', '<cmd>lua vim.lsp.buf.rename()<CR>')
+vim.keymap.set("n", "<Leader>r", "<cmd>lua vim.lsp.buf.rename()<CR>")
+vim.keymap.set("n", "<Leader>gg", "<cmd>Neogit<CR>")
