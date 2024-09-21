11f9b9d4a9f67440abd3571e3a3b5272b0be858e
Christian Belloni <christianbelloni@Christians-Laptop.local>
2024-09-21 23:35:26 +0200

    added spotify cmd

--- nvim/lua/config/mappings.lua
+++ nvim/lua/config/mappings.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n", "<Leader>r", "<cmd>lua vim.lsp.buf.rename()<CR>")
 vim.keymap.set("n", "<Leader>gg", "<cmd>Neogit<CR>")
+
+vim.keymap.set("n", "<Leader>sp", "<cmd>:terminal spt<CR>:startinsert<CR>")
